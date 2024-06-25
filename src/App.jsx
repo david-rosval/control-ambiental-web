199bc0a0068a6da50c795111fb2f642d6581c162
david-rosval <20b3010135@untels.edu.pe>
2024-06-25 12:51:55 -0500

    creando web control ambiental (front-end)

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+
+function App() {
+  useEffect(() => {
+    async function getSensorsData (){
+      const response = await fetch(import.meta.env.VITE_API_URL)
+      const data = await response.json()
+      const { temperature, humidity, ammonia } = data
+      setTemperature(temperature)
+      setHumidity(humidity)
+      setAmmonia(ammonia)
+    }
+    getSensorsData()
+  }, [])
+  
+  const [temperature, setTemperature] = useState(0)
+  const [humidity, setHumidity] = useState(0)
+  const [ammonia, setAmmonia] = useState(0)
+
+  return (
+    <>
+      <h1>Control ambiental</h1>
+      <div>
+        <div>
+          <p>temperature: {temperature}</p>
+        </div>
+        <div>
+          <p>temperature: {humidity}</p>
+        </div>
+        <div>
+          <p>temperature: {ammonia}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
